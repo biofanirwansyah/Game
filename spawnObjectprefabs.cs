66cb37e16cf654eb5dace57adeb6b875120419be
Bio Fanirwansyah <43460405+biofanirwansyah@users.noreply.github.com>
2018-11-23 11:00:06 +0700

    Add files via upload

---
+++ spawnObjectprefabs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnObjectprefabs : MonoBehaviour {
+
+    public GameObject cube;
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A))
+        {
+            spawnPrefabs();
+            Debug.Log("muncul");
+        }
+        else
+        {
+            Debug.Log("Gagal,tidak muncul");
+        }
+	}
+    
+    private void spawnPrefabs()
+    {
+        Instantiate(cube, transform.position, transform.rotation);
+    }
+}

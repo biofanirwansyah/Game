3984d560fda35b70f3577093ffb12062882ea450
Bio Fanirwansyah <43460405+biofanirwansyah@users.noreply.github.com>
2018-11-25 19:09:07 +0700

    Update spawnRandomObjectArray.cs

--- spawnRandomObjectArray.cs
+++ spawnRandomObjectArray.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class spawnRandomObjectArray : MonoBehaviour {
 
-    public GameObject[] objek;
-    public float minTime;
-    public float maxTime;
+    public GameObject[] objek; //variable untuk menjadi indikator array dari gameobject nanti
+    public float minTime;//rentan waktu minimal dari muncul nya game objek
+    public float maxTime;//rentan waktu maksimal dari muncul nya game objek
 
 
 
     void Start()
     {
+        //pemanggilan fungsi spawnObjek secara berulang dan secara acak(spawn gameobject dan posisi+rotasi)
         InvokeRepeating("SpawnDevil", Random.Range(minTime, maxTime), Random.Range(minTime, maxTime));
 
     }
 
 
-    void SpawnDevil()
+    void SpawnObjek()//fungsi untuk memunculkan gameobject yang dimasukkan kedalam Array
     {
         Instantiate(objek[Random.Range(0, objek.Length)], transform.position, transform.rotation);
     }

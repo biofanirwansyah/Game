451ef75a9672549aad645e096678548eb4c38e45
Bio Fanirwansyah <43460405+biofanirwansyah@users.noreply.github.com>
2018-11-25 19:05:04 +0700

    Add files via upload

---
+++ spawnRandomObjectArray.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnRandomObjectArray : MonoBehaviour {
+
+    public GameObject[] objek;
+    public float minTime;
+    public float maxTime;
+
+
+
+    void Start()
+    {
+        InvokeRepeating("SpawnDevil", Random.Range(minTime, maxTime), Random.Range(minTime, maxTime));
+
+    }
+
+
+    void SpawnDevil()
+    {
+        Instantiate(objek[Random.Range(0, objek.Length)], transform.position, transform.rotation);
+    }
+}
